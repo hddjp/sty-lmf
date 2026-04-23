d4d822ee10fb5ac4ac8f9cba2c13e8323c3d782e
hddjp <1059786998@qq.com>
2026-04-24 03:02:13 +0800

    424

--- ds.sh
+++ ds.sh
@@ -1,4 +1,4 @@
-rjob submit --gpu=8 --memory=1000000 -P 1 --cpu=128 --priority 8 --name=sft_random \
+rjob submit --gpu=8 --memory=1000000 -P 1 --cpu=128 --priority 8 --name=distill_random1 \
 --host-network=true \
 -e DISTRIBUTED_JOB=true \
 --charged-group=protfma_gpu --private-machine=group \
@@ -18,7 +18,57 @@ rjob submit --gpu=8 --memory=1000000 -P 1 --cpu=128 --priority 8 --name=sft_rand
   export HF_HUB_OFFLINE=1
   export OMP_NUM_THREADS=8
 
-  python -m llamafactory.cli train /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/train_scripts/sft_random.yaml
+  python -m llamafactory.cli train /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/train_scripts/distill_random1.yaml
   sudo chmod -R 777 /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves
-  rm -r /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves/qwen3-4b/sft_random/checkpoint-*/global_step*
+  rm -r /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves/qwen3-4b/distill_random1/checkpoint-*/global_step*
+"
+
+rjob submit --gpu=8 --memory=1000000 -P 1 --cpu=128 --priority 8 --name=distill_random2 \
+--host-network=true \
+-e DISTRIBUTED_JOB=true \
+--charged-group=protfma_gpu --private-machine=group \
+--custom-resources rdma/mlnx_shared=8 \
+--custom-resources mellanox.com/mlnx_rdma=1 \
+--mount=gpfs://gpfs2/ai4scifm-gpfs02:/mnt/shared-storage-gpfs2/ai4scifm-gpfs02 \
+--image=registry.h.pjlab.org.cn/ailab/pytorch2.7.0-cuda12.8.1-py3.12-ubuntu24.04:v2 \
+-- bash -c "
+  CONDA_PATH=/mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/miniconda3
+  . \${CONDA_PATH}/etc/profile.d/conda.sh && conda init && conda activate ssttyy
+
+  cd /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf
+  export NCCL_TIMEOUT=18000
+  export NCCL_DEBUG=INFO
+  export HF_DATASETS_CACHE=/mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/data/dataset_cache
+  export HF_DATASETS_OFFLINE=1
+  export HF_HUB_OFFLINE=1
+  export OMP_NUM_THREADS=8
+
+  python -m llamafactory.cli train /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/train_scripts/distill_random2.yaml
+  sudo chmod -R 777 /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves
+  rm -r /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves/qwen3-4b/distill_random2/checkpoint-*/global_step*
+"
+
+rjob submit --gpu=8 --memory=1000000 -P 1 --cpu=128 --priority 8 --name=distill_random3 \
+--host-network=true \
+-e DISTRIBUTED_JOB=true \
+--charged-group=protfma_gpu --private-machine=group \
+--custom-resources rdma/mlnx_shared=8 \
+--custom-resources mellanox.com/mlnx_rdma=1 \
+--mount=gpfs://gpfs2/ai4scifm-gpfs02:/mnt/shared-storage-gpfs2/ai4scifm-gpfs02 \
+--image=registry.h.pjlab.org.cn/ailab/pytorch2.7.0-cuda12.8.1-py3.12-ubuntu24.04:v2 \
+-- bash -c "
+  CONDA_PATH=/mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/miniconda3
+  . \${CONDA_PATH}/etc/profile.d/conda.sh && conda init && conda activate ssttyy
+
+  cd /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf
+  export NCCL_TIMEOUT=18000
+  export NCCL_DEBUG=INFO
+  export HF_DATASETS_CACHE=/mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/data/dataset_cache
+  export HF_DATASETS_OFFLINE=1
+  export HF_HUB_OFFLINE=1
+  export OMP_NUM_THREADS=8
+
+  python -m llamafactory.cli train /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/train_scripts/distill_random3.yaml
+  sudo chmod -R 777 /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves
+  rm -r /mnt/shared-storage-gpfs2/ai4scifm-gpfs02/wuyixin/code/sty-lmf/saves/qwen3-4b/distill_random3/checkpoint-*/global_step*
 "
